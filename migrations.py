e0eb9a380ca6fe23e9f291d52d0e31e7470f080f
Home PC <sarathm00@gmail.com>
2019-03-16 22:16:14 +0530

    Added all columns

--- migrations.py
+++ migrations.py
@@ -1,21 +1,67 @@
-from models import db, Contact
+from models import db, Asset
+import random
 from faker import Factory
-
+import datetime
 fake = Factory.create()
-# Spanish
-#fake = Factory.create('es_ES')
 # Reload tables
 db.drop_all()
 db.create_all()
-# Make 100 fake contacts
+# Make 100 fake Entries
+
+choice_list = {
+        'location' = ['neyyar', 'leela', 'gayathri'],
+        'processor' = ['i3', 'i5', 'i7'],
+        'ram' = ['4', '8', '16'],
+        'project_name' = ['tag', 'trc', 'delphi', 'hils']
+        }
+
+def random_alphanumeric(length):
+    temp = ''.join(random.choice('0123456789ABCDEF') for i in range(length=5))
+    return temp
+
+def random_date(start_date='today', end_date='+1y'):
+    # start_date = datetime.date(year=2015, month=1, day=1)
+    temp = fake.date_between(start_date=start_date, end_date=end_date)
+    return temp
+
 for num in range(100):
     fullname = fake.name().split()
     name = fullname[0]
-    surname = ' '.join(fullname[1:])
-    email = fake.email()
-    phone = fake.phone_number()
+    name = random.
+
+    emp_no = random.randint(1000, 2000)
+    location = random.choice(choice_list['location'])
+    seat_no = random.randint(100,200)
+    sl_no_1 = random.randint(100,200)
+    model = random_alphanumeric(5)
+    make = random_alphanumeric(5)
+    tel_tvm = random_alphanumeric(5)
+    test_pc = random_alphanumeric(5)
+    processor = random.choice(choice_list['processor'])
+    ram = random.choice(choice_list['ram'])
+    hdd = random.choice(choice_list['hdd'])
+    asset_no = random_alphanumeric()
+    allocation_date = random_date()
+    project_name = random.choice(choice_list['project_name'])
+    won_no = random.randint(1000,2000)
+    end_date = random_date(start_date=allocation_date, end_date='+1y')
     # Save in database
-    mi_contacto = Contact(name=name, surname=surname, email=email, phone=phone)
-    db.session.add(mi_contacto)
+    row_data = Asset(emp_no = emp_no,
+            location = location,
+            seat_no = seat_no,
+            sl_no_1 = sl_no_1,
+            model = model,
+            make = make,
+            tel_tvm = tel_tvm,
+            test_pc = test_pc,
+            processor = processor,
+            ram = ram,
+            hdd = hdd,
+            asset_no = asset_no,
+            allocation_date = allocation_date,
+            project_name = project_name,
+            won_no = won_no,
+            end_date = end_date )
+    db.session.add(row_data)
 
 db.session.commit()
--- models.py
+++ models.py
@@ -2,21 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///it_store.sqlite'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/book'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Contact(db.Model):
-
-
-    __tablename__ = 'contacts'
+class Asset(db.Model):
+    __tablename__ = 'asset_record'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    surname = db.Column(db.String(100), nullable=True)
-    email = db.Column(db.String(200), nullable=True, unique=True)
-    phone = db.Column(db.String(20), nullable=True, unique=False)
+    emp_no = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(20), nullable=True)
+    seat_no = db.Column(db.Integer, nullable=True, unique=False)
+    sl_no_1 = db.Column(db.String(20), nullable=True, unique=False)
+    model = db.Column(db.String(80), nullable=True, unique=False)
+    make = db.Column(db.String(20), nullable=True, unique=False)
+    tel_tvm = db.Column(db.String(20), nullable=True, unique=False)
+    test_pc = db.Column(db.String(20), nullable=True, unique=False)
+    processor = db.Column(db.String(20), nullable=True, unique=False)
+    ram = db.Column(db.String(20), nullable=True, unique=False)
+    hdd = db.Column(db.String(20), nullable=True, unique=False)
+    asset_no = db.Column(db.String(20), nullable=True, unique=False)
+    allocation_date = db.Column(db.String(20), nullable=True, unique=False)
+    project_name = db.Column(db.String(20), nullable=True, unique=False)
+    won_no = db.Column(db.String(20), nullable=True, unique=False)
+    end_date = db.Column(db.String(20), nullable=True, unique=False)
 
     def __repr__(self):
         return '<Contacts %r>' % self.name
